a63f6aaab0e022f027473dba72955e0cd12ffc00
Alencar da Costa <alencaradc@gmail.com>
2021-02-23 15:28:39 -0300

    Testing new TCP classes

--- src/client/app_client.cpp
+++ src/client/app_client.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include "lib/socket/TCPClient.hpp"
 
 int main() {
   std::cout << "Hello, World!" << std::endl;
+
+  TCPClient client = TCPClient();
+  TCPConnection* handle = client.connect("127.0.0.1", 8080);
+  std::cout << "Connected!\n";
+  std::string msg = "Client saying hi to server at port 8080";
+  handle->send(msg.c_str(), msg.size());
+
+  delete handle;
+
   return 0;
 }
--- src/server/app_server.cpp
+++ src/server/app_server.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 #include "lib/Thread.hpp"
+#include "lib/socket/TCPServer.hpp"
 
 class TestThread : public Thread {
   private:
@@ -15,5 +16,26 @@ int main() {
   TestThread* thread1 = new TestThread();
   thread1->start();
   thread1->join();
+
+  TCPServer server("127.0.0.1", 8080);
+  if (server.start() == false) {
+    std::cout << "Unable to start server!\n";
+    return 0;
+  }
+
+  TCPConnection* handle = server.accept();
+  if (handle == NULL) {
+    std::cout << "Unable to accept request!\n";
+    return 0;
+  }
+
+  char buffer[256];
+  int dataSize = handle->receive(buffer, sizeof(buffer));
+  std::cout << dataSize << " bytes received!\n";
+  buffer[dataSize] = '\0';
+  std::cout << "Received: " << std::string(buffer) << std::endl;
+
+  delete handle;
+
   return 0;
 }
